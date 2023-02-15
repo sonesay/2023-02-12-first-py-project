c1a2d78d4fc2b638e7a49530a4f7080d6790fcbc
Sone <sonesay.inthavong@maoritelevision.com>
2023-02-16 00:31:05 +1300

    Saving wip

--- helpers/db_conn.py
+++ helpers/db_conn.py
@@ -3,14 +3,14 @@ import sqlite3
 
 class DbConn:
     def __init__(self):
-        self.conn = sqlite3.connect('database.db')
+        self.conn = sqlite3.connect(r'C:\Users\sone\Desktop\mts6\php\php8-xdebug3-docker\web\database\database.sqlite')
         self.cursor = self.conn.cursor()
 
     def check_news_article_syncs(self, category):
         self.cursor.execute(
-            f"SELECT * FROM news_article_syncs WHERE category = '{category}' ORDER BY page DESC LIMIT 1;")
+            f"SELECT page FROM news_article_syncs WHERE category = '{category}' ORDER BY page DESC LIMIT 1;")
         row = self.cursor.fetchone()
         if row is not None:
-            return row[1]
+            return int(row[0])
         else:
             return None
--- helpers/web_scrapper.py
+++ helpers/web_scrapper.py
@@ -13,16 +13,25 @@ class WebScraper:
         for category, url in english_categories.items():
             highest_page = self.db_conn.check_news_article_syncs(category)
             if highest_page is not None:
-                # Continue scraping from last page
-                url += f'&page={highest_page}'
+                self.scrape_html_from_url(category, url)
             else:
-                # Start scraping from page 1
-                url += '&page=1'
+                self.scrape_html_from_url(category, url, highest_page, 20)
 
-            html = self.fetch_page(url)
-            parsed_data = self.parse_html(html)
-            # process the parsed data for this category here
-            print(f"Processed {category} category with data: {parsed_data}")
+    def scrape_html_from_url(self, category: str, url: str, page: int = 1, offset: int = 0) -> BeautifulSoup:
+        full_url = f"{url}&page={page}"
+        if offset > 0:
+            full_url += f"&offset={offset}"
+        response = requests.get(full_url)
+        html = response.text
+        soup = BeautifulSoup(html, "html.parser")
+        articles = soup.find_all("article")
+        for article in articles:
+            # process the article tag here
+            article_title = article.find("h3", class_="node-title").text.strip().replace("\n", "")
+            print(f"Processed article: {article_title}")
+
+        print(f"Processed {category} category with data: {soup}")
+        return soup
 
     def fetch_page(self, url):
         response = requests.get(url)
