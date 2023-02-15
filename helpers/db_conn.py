9df280315841829ba3eef91ff94e85602e64fa02
Sone <sonesay.inthavong@maoritelevision.com>
2023-02-16 12:15:38 +1300

    Fix logic for some bits

--- helpers/db_conn.py
+++ helpers/db_conn.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import datetime
 
 
 class DbConn:
@@ -14,3 +15,28 @@ class DbConn:
             return int(row[0])
         else:
             return None
+
+    def set_article(self, category, title, published_date, author, page, request_url, link):
+        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        # check if record already exists
+        self.cursor.execute(
+            f"SELECT id FROM news_article_syncs WHERE category = ? AND title = ?",
+            (category, title)
+        )
+        row = self.cursor.fetchone()
+        if row is not None:
+            # update existing record
+            self.cursor.execute(
+                f"UPDATE news_article_syncs SET article_datetime = ?, article_credits = ?, updated_at = ? WHERE id = ?",
+                (published_date, author, now, row[0])
+            )
+            self.conn.commit()
+        else:
+            # insert new record
+            if page is None:
+                page = 0
+            self.cursor.execute(
+                f"INSERT INTO news_article_syncs (category, title, published_date, author, page, request_url, link, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                (category, title, published_date, author, page or 0, request_url, link, now, now)
+            )
+            self.conn.commit()
--- helpers/web_scrapper.py
+++ helpers/web_scrapper.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 from helpers.db_conn import DbConn
@@ -13,22 +14,38 @@ class WebScraper:
         for category, url in english_categories.items():
             highest_page = self.db_conn.check_news_article_syncs(category)
             if highest_page is not None:
-                self.scrape_html_from_url(category, url)
-            else:
                 self.scrape_html_from_url(category, url, highest_page, 20)
+            else:
+                self.scrape_html_from_url(category, url)
 
-    def scrape_html_from_url(self, category: str, url: str, page: int = 1, offset: int = 0) -> BeautifulSoup:
+    def scrape_html_from_url(self, category: str, url: str, page: int = 0, offset: int = 0) -> BeautifulSoup:
         full_url = f"{url}&page={page}"
         if offset > 0:
             full_url += f"&offset={offset}"
+        print(f"full_url: {full_url}")
         response = requests.get(full_url)
         html = response.text
         soup = BeautifulSoup(html, "html.parser")
         articles = soup.find_all("article")
+        print(f"Found {len(articles)} articles")
         for article in articles:
             # process the article tag here
-            article_title = article.find("h3", class_="node-title").text.strip().replace("\n", "")
-            print(f"Processed article: {article_title}")
+            title = article.find("h3", class_="node-title").text.strip().replace("\n", "")
+            # extract credits
+            credits_div = article.find("div", class_="credits")
+            author = credits_div.text.strip() if credits_div else ""
+            if author.startswith('By '):
+                author = author.replace('By ', '', 1)
+            # extract datetime
+            datetime_tag = article.find("time", class_="time-ago")
+            published_date = datetime.strptime(datetime_tag['datetime'], '%Y-%m-%dT%H:%M:%S%z').strftime(
+                '%Y-%m-%d %H:%M:%S')
+            # extract article link
+            link = article.find("a", itemprop="mainEntityOfPage")['href']
+
+            self.db_conn.set_article(category, title, published_date, author, page, full_url, link)
+
+            print(f"Processed article: {published_date} - {title} - by {author} ")
 
         print(f"Processed {category} category with data: {soup}")
         return soup
