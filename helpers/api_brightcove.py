3dd88f9821beb2524499a2f7e03a4514ac7bfea3
Sonesay Inthavong <sonesay.inthavong@maoritelevision.com>
2023-02-21 13:44:29 +1300

    Saving wip

--- helpers/api_brightcove.py
+++ helpers/api_brightcove.py
@@ -1,44 +1,24 @@
-from oauthlib.oauth2 import WebApplicationClient
+from oauthlib.oauth2 import BackendApplicationClient
 import requests
-import urllib.parse
 
 
 class APIBrightcove:
-    def __init__(self, client_id, client_secret, access_token=None, redirect_uri=None):
+    def __init__(self, client_id, client_secret, access_token=None):
         self.client_id = client_id
         self.client_secret = client_secret
         self.access_token = access_token
         self.base_url = 'https://cms.api.brightcove.com/v1'
-        self.redirect_uri = redirect_uri or 'urn:ietf:wg:oauth:2.0:oob'
 
-        self.client = WebApplicationClient(self.client_id)
+        self.client = BackendApplicationClient(client_id=self.client_id)
 
     def authorize(self):
-        authorization_url, state = self.client.prepare_authorization_request(
-            'https://oauth.brightcove.com/v4/auth', redirect_uri=self.redirect_uri)
-
-        # Redirect the user to the authorization URL
-        # After the user grants permission, they will be redirected back to the redirect URI with an authorization code
-        response = requests.get(authorization_url)
-
-        # Extract the authorization code from the redirect URI
-        code = self.extract_authorization_code(response.url)
-
-        # Exchange the authorization code for an access token
         token_url = 'https://oauth.brightcove.com/v4/access_token'
-        self.client.prepare_token_request(token_url=token_url, authorization_response=response.url,
-                                          redirect_uri=self.redirect_uri)
-        token_response = self.client.send_token_request(token_url, auth=(self.client_id, self.client_secret),
-                                                        redirect_uri=self.redirect_uri)
 
-        self.access_token = token_response['access_token']
+        self.client.prepare_request_body()
+        token_response = requests.post(token_url, auth=(self.client_id, self.client_secret),
+                                       data=self.client.oauth2session.token)
 
-    def extract_authorization_code(self, url):
-        # Extract the query parameters from the URL
-        query_params = urllib.parse.parse_qs(urllib.parse.urlsplit(url).query)
-
-        # Extract the authorization code from the query parameters
-        return query_params.get('code', [None])[0]
+        self.access_token = token_response.json()['access_token']
 
     def get_video(self, video_id):
         headers = {'Authorization': f'Bearer {self.access_token}'}
